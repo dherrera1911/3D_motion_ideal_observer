21b34f588732232b01eebb5d831dd0826bc256f1
dherresp-adm <dherrera1911@gmail.com>
2024-04-30 18:39:32 -0400

    Add file to download data from osf

---
+++ data/download_training_data.sh
@@ -0,0 +1,13 @@
+
+# Download files from OSF repository
+
+# 3D speed, with looming
+osf -p w9mpe fetch osfstorage/AMA_input_stimuli/S3D-nStim_0500-spdStep_0.100-maxSpd_2.50-dspStd_00-dnK_2-loom_1-TRN.mat ama_inputs/S3D-nStim_0500-spdStep_0.100-maxSpd_2.50-dspStd_00-dnK_2-loom_1-TRN.mat
+
+osf -p w9mpe fetch osfstorage/AMA_input_stimuli/S3D-nStim_0300-spdStep_0.100-maxSpd_2.50-dspStd_00-dnK_2-loom_1-TST.mat ama_inputs/S3D-nStim_0300-spdStep_0.100-maxSpd_2.50-dspStd_00-dnK_2-loom_1-TST.mat
+
+# 3D direction, with looming
+osf -p w9mpe fetch osfstorage/AMA_input_stimuli/D3D-nStim_0500-spd_0.15-degStep_7.5-dspStd_00-dnK_2-loom_1-TRN.mat ama_inputs/D3D-nStim_0500-spd_0.15-degStep_7.5-dspStd_00-dnK_2-loom_1-TRN.mat
+
+osf -p w9mpe fetch osfstorage/AMA_input_stimuli/D3D-nStim_0300-spd_0.15-degStep_7.5-dspStd_00-dnK_2-loom_1-TST.mat ama_inputs/D3D-nStim_0300-spd_0.15-degStep_7.5-dspStd_00-dnK_2-loom_1-TST.mat
+

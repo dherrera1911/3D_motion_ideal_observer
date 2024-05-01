f64bb5ed182735e0969b7cd404700a3e8b098b05
dherresp-adm <dherrera1911@gmail.com>
2024-05-01 14:16:09 -0400

    Make cluster scripts executable

--- data/download_training_data.sh
+++ data/download_training_data.sh
@@ -1,5 +1,6 @@
-
 # Download files from OSF repository
+
+mkdir ama_inputs
 
 # 3D speed, with looming
 osf -p w9mpe fetch osfstorage/AMA_input_stimuli/S3D-nStim_0500-spdStep_0.100-maxSpd_2.50-dspStd_00-dnK_2-loom_1-TRN.mat ama_inputs/S3D-nStim_0500-spdStep_0.100-maxSpd_2.50-dspStd_00-dnK_2-loom_1-TRN.mat
